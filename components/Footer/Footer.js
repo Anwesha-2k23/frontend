d2a6ab9169edb5b05aa2419f7e9a90e27dc316d4
Aritra Bhaduri <92646038+CodingwithMe123@users.noreply.github.com>
2022-11-12 02:59:57 +0530

    updated footer and coming soon page

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,19 +5,21 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.icon_links}>
-        <Link className={styles.icon_link} href=""> Facebook</Link>   {/* Facebook Link Here */}
-        <Link className={styles.icon_link} href="">Instagram</Link>  {/* Instagram Link Here */}
-        <Link className={styles.icon_link} href="">Youtube</Link> {/* Youtube Link Here */}
+      <div className={[styles.icon_links, styles.linkGroup].join(" ")}>
+        <Link className={[styles.icon_link, styles.link].join(" ")} href=""> Facebook</Link>   {/* Facebook Link Here */}
+        <Link className={[styles.icon_link, styles.link].join(" ")} href="">Instagram</Link>  {/* Instagram Link Here */}
+        <Link className={[styles.icon_link, styles.link].join(" ")} href="">Youtube</Link>  {/* Youtube Link Here */}
       </div>
-      <div className={styles.footer_links}>
-        <Link className={styles.footer_link} href="">Info</Link>
-        <Link className={styles.footer_link} href="">Support</Link>
-        <Link className={styles.footer_link} href="">Marketing</Link>
+      <hr className={styles.footerSeperator}/>
+      <div className={[styles.footer_links, styles.linkGroup].join(" ")}>
+        <Link className={[styles.footer_link, styles.link].join(" ")} href="">Info</Link>
+        <Link className={[styles.footer_link, styles.link].join(" ")} href="">Support</Link>
+        <Link className={[styles.footer_link, styles.link].join(" ")} href="">Marketing</Link>
       </div>
-      <div className={styles.tac_links}>
-        <Link className={styles.tac_link} href="">Terms Of Use</Link>
-        <Link className={styles.tac_link} href="">Privacy Policy</Link>
+      <hr className={styles.footerSeperator}/>
+      <div className={[styles.tac_links, styles.linkGroup].join(" ")}>
+        <Link className={[styles.tac_link, styles.link].join(" ")} href="">Terms Of Use</Link>
+        <Link className={[styles.tac_link, styles.link].join(" ")} href="">Privacy Policy</Link>
       </div>
     </div>
   )
