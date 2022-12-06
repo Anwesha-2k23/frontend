264caaa8fd93d50352060b34003b8133730a7292
Anurag Deo <anuragdeo03@gmail.com>
2022-12-06 16:37:11 +0530

    Changed the homepage and added the form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 import styles from "../styles/comingsoon.module.css";
 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 export default function comingsoon() {
+  useEffect(() => {
+    
+    var d = document
+    var w="https://tally.so/widgets/embed.js"
+    var v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if(d.querySelector('script[src="'+w+'"]'))v();else{var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
+  
+    return () => {
+      
+    }
+  }, [])
+  
   return (
     <div className={styles.comingsoon_body}>
       <Head>
@@ -21,15 +33,46 @@ export default function comingsoon() {
       <div className={styles.socials}>
         <div className={styles.imageElement}>
           <Link href="https://instagram.com/anwesha.iitp?igshid=YmMyMTA2M2Y=" target="_blank">
-            <Image src="/instalogo.png" width={50} height={50} />
+            <Image src="/instagram.svg" width={50} height={50} />
           </Link>
         </div>
         <div className={styles.imageElement}>
           <Link href="https://m.youtube.com/@AnweshaIITP?itct=CBgQq6cCIhMIv5uekI6m-wIVKcmgAh3FlAur" target="_blank">
-            <Image src="/ytlogo.png" width={50} height={50} />
+            <Image src="/youtube.svg" width={50} height={50} />
+          </Link>
+        </div>
+        <div className={styles.imageElement}>
+          <Link href="https://twitter.com/anweshaiitpat" target="_blank">
+            <Image src="/twitter.svg" width={50} height={50} />
+          </Link>
+        </div>
+        <div className={styles.imageElement}>
+          <Link href="https://www.facebook.com/anwesha.iitpatna/" target="_blank">
+            <Image src="/facebook.svg" width={50} height={50} />
           </Link>
         </div>
       </div>
+
+      {/* Campus Ambassador */}
+      <div className={styles.ca}>
+      <div className={styles.ca_poster}>
+      </div>
+      <div className={styles.ca_info}>
+        <h1 className={styles.ca_heading}>Campus Ambassador</h1>
+        <p className={styles.ca_text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis expedita voluptas vitae accusantium animi. Labore perferendis rem placeat quod corporis quas eius quibusdam id, veniam illo aut mollitia dolore doloremque cupiditate, debitis quos molestiae totam! Eligendi ad reprehenderit, exercitationem iusto, vero corrupti quibusdam animi odio, praesentium delectus obcaecati debitis! Reiciendis amet natus atque eum harum nobis, iste ad repellendus voluptate odio deleniti dolorem veniam temporibus distinctio voluptates assumenda aspernatur! Perferendis dicta libero atque officia quaerat maxime nam</p>
+      </div>
+      </div>
+
+      {/* Anwesha Dispatch */}
+      <div className={styles.dispatch_form}>
+      <iframe data-tally-src="https://tally.so/embed/mRW0rJ?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" width="100%" height="1326" frameborder="0" marginheight="0" marginwidth="0" title="Anwesha Dispatch">
+      </iframe>
+      
+      </div>
+      {/* <script>
+      var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if(d.querySelector('script[src="'+w+'"]'))v();else{var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
+      </script> */}
+
       {/* <Footer/> */}
     </div>
   );
