8499bde0c1466e3d2cff7d90adb811c6db131249
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2022-12-06 20:46:53 +0530

    added the gallery page

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,33 +1,44 @@
 import style from './Gallery.module.css'
-
-const Gallery = () => {
+const Gallery = (props) => {
   return (
-    <div className={style.gallery}>
+    <div className={style.event}>
+      <h1>{props.eventName}</h1>
+      <p className={style.description}>{props.desc}</p>
+      <div className={style.gallery}>
     <figure className={`${style.galleryItem} ${style.galleryItem1}`}>
-      {/* <img src="img/image-1.jpg" className={style.galleryImg} alt="Image 1"></img> */}
-      <div className={style.galleryImg}></div>
+      <div className={style.galleryImg}>
+      <img src={`${props.image1}`} className={style.galleryImg} alt="Image 1"></img>
+      </div>
     </figure>
     <figure className={`${style.galleryItem} ${style.galleryItem2}`}>
-      {/* <img src="img/image-2.jpg" className={style.galleryImg} alt="Image 2"></img> */}
-            <div className={style.galleryImg}></div>
+        <div className={style.galleryImg}>
+      <img src={`${props.image2}`} className={style.galleryImg} alt="Image 2"></img>
+        </div>
     </figure>
     <figure className={`${style.galleryItem} ${style.galleryItem3}`}>
-      {/* <img src="img/image-3.jpg" className={style.galleryImg} alt="Image 3"></img> */}
-            <div className={style.galleryImg}></div>
+            <div className={style.galleryImg}>
+      <img src={`${props.image3}`} className={style.galleryImg} alt="Image 3"></img>
+
+            </div>
     </figure>
     <figure className={`${style.galleryItem} ${style.galleryItem4}`}>
-      {/* <img src="img/image-4.jpg" className={style.galleryImg} alt="Image 4"></img> */}
-            <div className={style.galleryImg}></div>
+            <div className={style.galleryImg}>
+      <img src={`${props.image4}`} className={style.galleryImg} alt="Image 4"></img>
+            </div>
     </figure>
     <figure className={`${style.galleryItem} ${style.galleryItem5}`}>
-      {/* <img src="img/image-5.jpg" className={style.galleryImg} alt="Image 5"></img> */}
-            <div className={style.galleryImg}></div>
+            <div className={style.galleryImg}>
+      <img src={`${props.image5}`} className={style.galleryImg} alt="Image 5"></img>
+            </div>
     </figure>
     <figure className={`${style.galleryItem} ${style.galleryItem6}`}>
-      {/* <img src="img/image-6.jpg" className={style.galleryImg} alt="Image 6"></img> */}
-            <div className={style.galleryImg}></div>
+            <div className={style.galleryImg}>
+      <img src={`${props.image6}`} className={style.galleryImg} alt="Image 6"></img>
+            </div>
     </figure>
   </div>
+    </div>
+    
   )
 }
 
