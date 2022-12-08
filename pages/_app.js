3a53445fa68b6f2c2076dbaa58ea9b3eeda89318
Komachian <adiljamal221101@gmail.com>
2022-12-08 11:24:04 +0530

    Google Analytics Added to All Pages

--- pages/_app.js
+++ pages/_app.js
@@ -10,18 +10,9 @@ const poppins = Poppins({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <script async src={"https://www.googletagmanager.com/gtag/js?id=" + process.env.GOOGLE_ANALYTICS}></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-        gtag('config', process.env.GOOGLE_ANALYTICS);
-      </script>
-      <main className={roboto.className}>
-        <Component {...pageProps} />
-      </main>
-    </>
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
   )
 }
 
