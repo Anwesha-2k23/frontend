434b2af28ffe08b64684430c41c9df590e6c809e
Adil Jamal <adiljamal221101@gmail.com>
2022-12-08 11:10:12 +0530

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,18 @@ const poppins = Poppins({
 
 function MyApp({ Component, pageProps }) {
   return (
-  <main className={roboto.className}>
-    <Component {...pageProps} />
-  </main>
+    <>
+      <script async src={"https://www.googletagmanager.com/gtag/js?id=" + process.env.GOOGLE_ANALYTICS}></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', process.env.GOOGLE_ANALYTICS);
+      </script>
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
 
