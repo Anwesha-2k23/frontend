2c3b96d488df29de1c57c93c02dedfcf7d0b64af
Komachian <adiljamal221101@gmail.com>
2022-12-08 11:39:53 +0530

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,19 @@ const poppins = Poppins({
 function MyApp({ Component, pageProps }) {
   return (
     <main className={roboto.className}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.GOOGLE_ANALYTICS});
+        `}
+      </Script>
       <Component {...pageProps} />
     </main>
   )
