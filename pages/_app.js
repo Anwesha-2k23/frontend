0bd2026f0695f39331a565b032cf6d0a2813aa01
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-10 12:31:38 +0530

    last

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Roboto, Poppins } from "@next/font/google";
+import Head from 'next/head';
 
 const roboto = Roboto({
   weight: "400",
@@ -11,6 +12,12 @@ const poppins = Poppins({
 function MyApp({ Component, pageProps }) {
   return (
   <main className={roboto.className}>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    </Head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></script>
+
     <Component {...pageProps} />
   </main>
   )
--- pages/nav.js
+++ pages/nav.js
@@ -9,7 +9,6 @@ const Navbar = () => {
             <Head>
                 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
             </Head>
             <nav className={`${styles.whole} navbar fixed-top`} >
@@ -30,21 +29,27 @@ const Navbar = () => {
                     <h4 className={styles.h8}>Login</h4>
 
 
-                    <img src="./navbar/logo.svg" className={styles.navbar_2} />
-                    <img src="./navbar/nav bar.svg" className={styles.navbar_1} />
                     <img src="./navbar/navbar mandala left.svg" className={styles.navbar_3} />
                     <img src="./navbar/navbar mandala right.svg" className={styles.navbar_4} />
+                    <img src="./navbar/logo.svg" className={styles.navbar_2} />
+
+                    <img src="./navbar/nav bar.svg" className={styles.navbar_1} />
+
                 </div>
+
 
 
 
             </nav>
 
 
+            <div>
+            </div>
+
 
             {/* </form> */}
             <div className={styles.menu}>
-                <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div className="offcanvas-header">
                         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
