2e03d071ba6f09cd8d3d6115e6b92ea75a7b43bb
aaryaa <maharajaaryaa@gmail.com>
2022-12-10 12:44:02 +0530

    added leaderboard

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -12,60 +12,62 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Leaderboard() {
   const [searchInput, setSearchInput] = useState("");
   return (
-    <div className="container">
-      <h2 className="my-5" style={{ textAlign: "center" }}>
-        LEADERBOARD
-      </h2>
+    <div className={styles.anwesha_bg_img}>
       <div className="container">
-        <Head>
-          <title>Leaderboard - Anwesha 2023</title>
-          <meta name="description" content="Anwesha 2023" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-      </div>
-      <div className="container">
-        <div>
-          <InputGroup className="my-3">
-            <Form.Control
-              type="search"
-              onChange={(event) => setSearchInput(event.target.value)}
-              value={searchInput}
-              placeholder="Search for Participants"
-            />
-          </InputGroup>
+        <h2 className="my-5" style={{ textAlign: "center" }}>
+          LEADERBOARD
+        </h2>
+        <div className="container">
+          <Head>
+            <title>Leaderboard - Anwesha 2023</title>
+            <meta name="description" content="Anwesha 2023" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
         </div>
-
-        <Table hover responsive>
-          <thead>
-            <tr>
-              <th>Position</th>
-              <th>Name</th>
-              <th>College</th>
-              <th>Points</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {Items.filter((post) => {
-              if (searchInput === "") {
-                return post;
-              } else if (
-                post.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-                post.college.toLowerCase().includes(searchInput.toLowerCase())
-              ) {
-                return post;
-              }
-            }).map((post) => (
-              <LeaderboardItem
-                key={post.position}
-                position={post.position}
-                name={post.name}
-                college={post.college}
-                points={post.points}
+        <div className="container">
+          <div>
+            <InputGroup className="my-3">
+              <Form.Control
+                type="search"
+                onChange={(event) => setSearchInput(event.target.value)}
+                value={searchInput}
+                placeholder="Search for Participants"
               />
-            ))}
-          </tbody>
-        </Table>
+            </InputGroup>
+          </div>
+
+          <Table striped responsive>
+            <thead>
+              <tr className={styles.heading_color}>
+                <th>Position</th>
+                <th>Name</th>
+                <th>College</th>
+                <th>Points</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {Items.filter((post) => {
+                if (searchInput === "") {
+                  return post;
+                } else if (
+                  post.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+                  post.college.toLowerCase().includes(searchInput.toLowerCase())
+                ) {
+                  return post;
+                }
+              }).map((post) => (
+                <LeaderboardItem
+                  key={post.position}
+                  position={post.position}
+                  name={post.name}
+                  college={post.college}
+                  points={post.points}
+                />
+              ))}
+            </tbody>
+          </Table>
+        </div>
       </div>
     </div>
   );
