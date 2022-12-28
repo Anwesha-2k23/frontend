291571f1418cc224d9df2eb708bb3225a9a5936f
melencholicmice <learningwithturtlespeedcode@gmail.com>
2022-12-28 08:29:31 +0530

    footer changes made

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import styles from './Footer.module.css'
-import Image from 'next/image'
+import Image from "next/image";
 import { faceBookSVG, InstagramSVG, YouTubeSVG, TwitterSVG } from './svgs'
 
 
@@ -9,17 +9,28 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={[styles.mainSection].join(" ")}>
         <div className={[styles.section3].join(" ")}>
-          <div className={[styles.section3maintext].join(" ")}>Anwesha</div>
-          <p className={[styles.section3text].join(" ")}>Lorem sed do eius</p>
+          {/* <div className={[styles.section3maintext].join(" ")}>Anwesha</div>
+           */}
+          <div className={styles.anwesha_text}>
+            <img src="/Anwesha_text.png" alt="Anwesha" className={styles.anwesha_text_img} />
+            {/* <h1 className={styles.bgText}>COMING SOON...</h1> */}
+          </div>
+          {/* <p className={[styles.section3text].join(" ")}>Lorem sed do eius</p> */}
           <div className={[styles.icon_links, styles.linkGroup].join(" ")}>
             {/* <div> */}
-            <Link className={[styles.icon_link, styles.link].join(" ")} href="">{faceBookSVG}</Link> {/* Facebook Link Here */}
+            <Link className={[styles.icon_link, styles.link].join(" ")}
+              href="https://www.facebook.com/anwesha.iitpatna/">{faceBookSVG}</Link> {/* Facebook Link Here */}
             <Link className={[styles.icon_link, styles.link].join(" ")} href="https://instagram.com/anwesha.iitp?igshid=YmMyMTA2M2Y=">{InstagramSVG}</Link>
             {/* </div> Instagram Link Here */}
             {/* <div> */}
             <Link className={[styles.icon_link, styles.link].join(" ")} href="https://m.youtube.com/@AnweshaIITP?itct=CBgQq6cCIhMIv5uekI6m-wIVKcmgAh3FlAur">{YouTubeSVG}</Link>  {/* Youtube Link Here */}
-            <Link className={[styles.icon_link, styles.link].join(" ")} href="/">{TwitterSVG}</Link>  {/* Youtube Link Here */}
+            <Link className={[styles.icon_link, styles.link].join(" ")}
+              href="https://twitter.com/anweshaiitpat/">{TwitterSVG}</Link>  {/* Youtube Link Here */}
             {/* </div> */}
+          </div>
+          <div className={[styles.button_links, styles.linkGroup,styles.googlePlayButton].join(" ")}>
+            {/* <div> */}
+            <a href=""><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' /></a>
           </div>
         </div>
 
