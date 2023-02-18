6047bc4463923cfb5d9877649b469820b8e80945
aritroCoder <ogcodinggod@gmail.com>
2023-02-18 14:59:11 +0530

    reverted useless commits

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     images: {
-        domains: ['images.unsplash.com'],
     },
 }
 
