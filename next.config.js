caf5cf27a8e0907dac399eec84efd82588932410
aritroCoder <ogcodinggod@gmail.com>
2023-02-18 14:59:27 +0530

    revert commits

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
+        domains: ['images.unsplash.com'],
     },
 }
-
 
 module.exports = nextConfig
