08c53b66df81ca38847adfcff810bb059b7cbf7f
aritroCoder <ogcodinggod@gmail.com>
2023-03-02 19:45:49 +0530

    added admin rive

---
+++ components/Rive/DisplayRiveAdmin.js
@@ -0,0 +1,22 @@
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas'
+
+export default function Displayrive() {
+    const { rive, RiveComponent } = useRive({
+        src: "Anwesha animations.riv",
+        artboard: 'Admin isle.png',
+        stateMachines: "Anwesha Register component",
+        autoplay: true,
+    })
+    const onHoverInput = useStateMachineInput(
+        rive,
+        'Anwesha Register component',
+        'Hover on Register'
+    )
+
+    return <RiveComponent onClick={() => onHoverInput.fire()} style={{
+      position: 'absolute',
+      top: '200px',
+      height: '750px',
+     
+    }} />
+}
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import HomeBackgroundAnimation from '../components/Rive/homeBackgrounAnim'
 import styles from '../styles/homepage.module.css'
 import DisplayRiveAnwesha from '../components/Rive/DisplayRiveAnwesha'
+import DisplayRiveAdmin from '../components/Rive/DisplayRiveAdmin'
 
 const index = () => {
   return (
     <div className={styles.parentcontainer}>
         <DisplayRiveAnwesha/>
+        <DisplayRiveAdmin/>
     </div>
   )
 }
