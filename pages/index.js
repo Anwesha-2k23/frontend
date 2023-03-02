ff31e97d0d344d7079792bac921923f9847b39ea
Anuag-Deo <anuragdeo03@gmail.com>
2023-03-02 20:16:11 +0530

    Added responsiveness

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeBackgroundAnimation from '../components/Rive/homeBackgrounAnim'
 import styles from '../styles/homepage.module.css'
 import DisplayRiveAnwesha from '../components/Rive/DisplayRiveAnwesha'
 import DisplayRiveAdmin from '../components/Rive/DisplayRiveAdmin'
 
 const index = () => {
+  const [isDesktop, setDesktop] = useState(true);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 1550);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
   return (
     <div className={styles.parentcontainer}>
-        <DisplayRiveAnwesha/>
-        <DisplayRiveAdmin/>
-        <HomeBackgroundAnimation riveUrl="/Anwesha_animations.riv" artboardName={'SAC'} styling={{ height: '500px', width: '500px', position: 'absolute', top: '40%', left: '2%' }} />
-        <HomeBackgroundAnimation riveUrl="/Anwesha_animations.riv" artboardName={'Gymkhana'} styling={{ height: '350px', width: '350px', position: 'absolute', top: '50%', left: '75%' }} />
+      {isDesktop ? (
+        <div className={styles.container}>
+          <DisplayRiveAnwesha />
+          <DisplayRiveAdmin />
+          <HomeBackgroundAnimation riveUrl="/Anwesha_animations.riv" artboardName={'SAC'} styling={{ height: '500px', width: '500px', position: 'absolute', top: '40%', left: '2%' }} />
+          <HomeBackgroundAnimation riveUrl="/Anwesha_animations.riv" artboardName={'Gymkhana'} styling={{ height: '350px', width: '350px', position: 'absolute', top: '50%', left: '75%' }} />
+
+        </div>
+      ) : (
+        <div className={styles.container}>
+          <DisplayRiveAnwesha />
+          <DisplayRiveAdmin />
+
+        </div>
+      )
+      }
     </div>
   )
 }
