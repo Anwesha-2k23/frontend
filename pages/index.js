1be5bd625765498c65cf9ee6c7c9fcc301e3f90f
aritroCoder <ogcodinggod@gmail.com>
2023-03-02 23:22:54 +0530

    Changes to home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Image from 'next/image';
 import HomeBackgroundAnimation from '../components/Rive/homeBackgrounAnim'
 import styles from '../styles/homepage.module.css'
 import DisplayRiveAnwesha from '../components/Rive/DisplayRiveAnwesha'
@@ -9,17 +10,18 @@ import DisplayRiveGymkhana from '../components/Rive/DisplayRiveGymkhana'
 const index = () => {
 
   return (
+    <>
     <div className={styles.parentcontainer}>
-
         <div className={styles.container}>
           <DisplayRiveAnwesha />
           <DisplayRiveAdmin />
           <DisplayRiveSAC />
           <DisplayRiveGymkhana />
-
+          <Image src="/home/Lecture_hall_island.png" alt="Lecture hall island" width={141} height={144} className={styles.lecturehall}/>
         </div>
 
     </div>
+    </>
   )
 }
 
