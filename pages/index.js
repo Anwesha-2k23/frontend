9bb84687261a652b45eae43952cafe5e809dcaa3
aritroCoder <ogcodinggod@gmail.com>
2023-03-04 23:14:52 +0530

    Hopefully finalized the home page

--- pages/index.js
+++ pages/index.js
@@ -70,17 +70,22 @@ const index = () => {
                             alt="Events image"
                         />
                         <div className={styles.eventSectionText}>
-                            Step into a world where dreams come true and
-                            possibilities are endless. The Isles of Utopia
-                            await, where every individual is free to thrive and
-                            prosper, and the pursuit of happiness is a shared
-                            goal. It's a world where compassion and empathy
-                            reign, and social justice is the foundation of
-                            society. Come explore the idyllic landscapes, bask
-                            in the warm glow of the sun, and feel the gentle
-                            breeze on your face. The Isles of Utopia offer a
-                            glimpse of a better world, a world where anything is
-                            possible, and everyone is welcome
+                            Anwesha, the Annual Cultural-Management Fest of IIT
+                            Patna, is grandeur host of magnificent and thrilling
+                            events. We have everything starting from our legacy
+                            events including Syngphony, Heelturn to our most
+                            exhilarating title event, Mr/Ms Anwesha. We have
+                            Verve, the fashion walk event, stretching our
+                            platform for all the fashion influencers. We bring
+                            you many unconventional events like StepUp, Graffiti
+                            Workshop, Imagination Station and monochrome as well
+                            as a first of its kind AnimeCon, an evening full of
+                            anime for all anime lovers out there, Anwesha have
+                            got you’ll covered. Just enter the arena and explore
+                            every isles you’ve ever imagined about. Make sure
+                            you create your Anwesha ID and swim into the pool of
+                            events, check out the rulebooks and feel free to
+                            contact the organizers for all doubts/queries.
                             <br />
                             <br />
                             <div style={{ textAlign: 'center', width: '100%' }}>
@@ -193,23 +198,12 @@ const index = () => {
                             alt="Events image"
                         />
                         <div className={styles.eventSectionText}>
-                            Step into a world where dreams come true and
-                            possibilities are endless. The Isles of Utopia
-                            await, where every individual is free to thrive and
-                            prosper, and the pursuit of happiness is a shared
-                            goal. It's a world where compassion and empathy
-                            reign, and social justice is the foundation of
-                            society. Come explore the idyllic landscapes, bask
-                            in the warm glow of the sun, and feel the gentle
-                            breeze on your face. The Isles of Utopia offer a
-                            glimpse of a better world, a world where anything is
-                            possible, and everyone is welcome
-                            <br />
+                             Anwesha 2023 is envisioned to be an innovative and cultural uplifting festival of togetherness in not just the state or region, but the whole nation. We have reimagined the lens through which we understand culture and celebration, deriving the cornerstones for this year's edition that shall drive forward our festival.                           <br />
                             <br />
                             <div style={{ textAlign: 'center', width: '100%' }}>
                                 <button className={styles.homebtn}>
                                     <Link
-                                        href="/events"
+                                        href="/aboutus"
                                         style={{
                                             textDecoration: 'none',
                                             color: '#fff',
@@ -224,6 +218,55 @@ const index = () => {
                 </div>
             </div>
             {/*End about us section*/}
+
+            {/*Team section*/}
+            <div className={styles.themeSection}>
+                <div className={styles.themeSectionContainer}>
+                    <Image
+                        src="/home/teams_island.svg"
+                        width={535}
+                        height={395}
+                        alt="Events section"
+                    />
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            maxWidth: '600px',
+                            position: 'relative',
+                            boxShadow: '#000000 7px 7px 0px',
+                            marginBottom: '10px',
+                            borderBottomLeftRadius: '15px',
+                            borderBottomRightRadius: '15px',
+                        }}
+                    >
+                        <img
+                            src="/home/team.svg"
+                            layout="fill"
+                            width="100%"
+                            objectFit="contain"
+                            alt="Events image"
+                        />
+
+                        <div className={styles.themeSectionText}>
+                           The grandeur of Anwesha is something that requires a powerful community to come together alongwith their thrilling ideas and dedicated promises. The Anwesha community comprises of seven different committees including Events and Planning, Media, Public and Relations, Hospitality, Registration, Security and Planning, Creatives and Design, Web and App Development and Sponorship committee. It takes a whole year of these dedicated committee members to pull off Anwesha on a bigger scale ever imagined by our guests and participants.
+                            <br />
+                            <br />
+                            <div style={{ textAlign: 'center', color: '#fff' }}>
+                                <button className={styles.homebtn}>
+                                    <Link href="/ourteam" style={{ color: '#fff' }}>
+                                        Meet the Team
+                                    </Link>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*End team section*/}
+
 
             <div style={{ height: '101px' }}></div>
         </>
