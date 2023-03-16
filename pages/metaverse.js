3c1cd83c6bbd744dbc6958a04b65fb827c817f41
Komachian <adiljamal221101@gmail.com>
2023-03-16 23:28:22 +0530

    Dinesh Fucked Up Last time

--- pages/metaverse.js
+++ pages/metaverse.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from '../styles/metaverse.module.css'
 import UserRegisterForm from '../components/UserRegister-Login'
 import React from 'react'
+import dynamic from 'next/dynamic'
+
 const Iframe = dynamic(() => import('../components/lazyIframe'), { ssr: false })
 
 export default function userRegister() {
