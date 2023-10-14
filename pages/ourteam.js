7450ecc51ebda0380937ffd6717cf37763dc0794
Harshvardhan <118089906+DEDSWIN@users.noreply.github.com>
2023-10-14 16:31:41 +0530

    Teams Page Updated for 2023 Team (#93)
    * Teams Page Updated
    
    * Optimized Coord Images
    
    ---------
    
    Co-authored-by: Genusuppal <harpranav2202@gmail.com>

--- pages/ourteam.js
+++ pages/ourteam.js
@@ -1,52 +1,115 @@
 import Image from 'next/image'
+import Head from 'next/head'
 import React from 'react'
 import styles from '../styles/ourteam.module.css'
 
-const ourteam = () => {
+function CoordCard({ data, title }) {
+    return (
+        <>
+            <h2 className={styles.teamSubHeading}>{title}</h2>
+            {/* <hr className={styles.teamSubHeadingBar} /> */}
+            <div className={styles.picContainer}>
+                {data.map((item, index) => (
+                    <div key={index} className={styles.teamCard}>
+                        <Image
+                            className={styles.memberImage}
+                            height={220}
+                            width={220}
+                            src={item.image}
+                            alt={item.name}
+                        />
+                        <div className={styles.teamMemberDetails}>
+                            <div className={styles.memberName}>
+                                <strong>{item.name}</strong>
+                            </div>
+                            {/* <div className={styles.memberCommitte}>
+                                {item.committee}
+                            </div> */}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>)
+}
+
+export default function ourteam() {
     // const n = 4;
     const data = {
-        coordinators: [
-            { name: 'Anurag', committee: 'WebnApp', image: '/A_logo.png' },
-            { name: 'Aritra', committee: 'WebnApp', image: '/A_logo.png' },
-            { name: 'Praveen', committee: 'WebnApp', image: '/A_logo.png' },
-            { name: 'Divit', committee: 'WebnApp', image: '/A_logo.png' },
-            { name: 'Divit', committee: 'WebnApp', image: '/A_logo.png' },
+        festcoord: [
+            { name: 'Abhilasha', image: '/team/abhilasha.png' },
+            { name: 'Yashweer', image: '/team/yashveer.png' }
+        ],
+        webnapp: [
+            { name: 'Anurag Deo', image: '/team/anurag.png' },
+            { name: 'Aritra Bhaduri', image: '/team/aritra.png' },
+            { name: 'Vaibhav Gupta', image: '/team/vaibhav.jpg' },
+            { name: 'Arvind Meena', image: '/team/arvind.jpg' },
+        ],
+        spons: [
+            { name: 'Suryansh Bansal', image: '/team/suryansh_spons.jpg' },
+            { name: 'Aviral Shrivastava', image: '/team/aviral_spons.jpg' },
+            { name: 'Ritvik', image: '/team/ritvik_spons.png' },
+
+        ],
+        TV: [
+            { name: 'Arkadeep Acharya', image: '/team/akradeep_tv.jpg' },
+            { name: 'Diptadip Malik', image: '/team/diptadip_tv.jpg' },
+            { name: 'Surya Prakash', image: '/team/surya_tv.jpg' },
+            { name: 'Ankit Kumar', image: '/team/ankit_tv.jpg' },
+
+        ],
+        events: [
+            { name: 'Aryan', image: '/team/aryan_events.jpg' },
+            { name: 'Divyanshee', image: '/team/divyanshee_events.png' },
+            { name: 'Shivendra', image: '/team/shivendra_events.jpg' },
+            { name: 'Yash', image: '/team/yash_events.jpg' },
+            { name: 'Nikita', image: '/team/nikita_events.jpg' },
+            
+        ],
+        rsp: [
+            { name: 'Khushi Pathak', image: '/team/khushi_rsp.jpeg' },
+            { name: 'Harsh Chandra', image: '/team/harsh_rsp.jpeg' },
+            { name: 'Swabnam', image: '/team/swabnam_rsp.jpeg' },
+            { name: 'Sudhanshu', image: '/team/sudhanshu_rsp.jpeg' },
+
+        ],
+        hospi: [
+            { name: 'Aditya Pandey', image: '/team/aditya_hospi.jpeg' },
+            { name: 'Aditi Kumari', image: '/team/aditi_hospi.jpg' },
+            { name: 'Aruj Gautam', image: '/team/aruj_hospi.jpeg' },
+            { name: 'Devendra Singh', image: '/team/devendra_hospi.jpeg' },
+
+        ],
+        mpr: [
+            { name: 'Kumari Ankita', image: '/team/ankita_mpr.jpg' },
+            { name: 'Gaurang Bansal', image: '/team/gaurang_mpr.jpg' },
+            { name: 'Ankur Kumar', image: '/team/ankur_mpr.jpg' },
+            { name: 'Anudeep Talari', image: '/team/anudeep_mpr.jpg' },
+            { name: 'Kanishka Solanki', image: '/team/kanishka_mpr.jpg' },
         ],
     }
     return (
         <div className={styles.parentContainer}>
+		<Head>
+			<title>Team - Anwesha 2023</title>
+			<meta name="description" content="Team-Anwesha 2023" />
+			<link rel="icon" href="./logo_no_bg.svg" />
+		</Head>
             <div className={styles.container}>
                 <h2 className={styles.teamHeader}>Our Team</h2>
                 <hr className={styles.teamHeadingBar} />
                 <div className={styles.coordinators}>
-                    <h2 className={styles.teamSubHeading}>Coordinators</h2>
-                    <hr className={styles.teamSubHeadingBar} />
-                    <div className={styles.picContainer}>
-                        {/* {[...Array(n)].map((e, i) =>  */}
-                        {data.coordinators.map((item, index) => (
-                            <div className={styles.teamCard}>
-                                <Image
-                                    className={styles.memberImage}
-                                    height={220}
-                                    width={220}
-                                    src={`${item.image}`}
-                                    alt={item.name}
-                                />
-                                <div className={styles.teamMemberDetails}>
-                                    <div className={styles.memberName}>
-                                        <strong>{item.name}</strong>
-                                    </div>
-                                    <div className={styles.memberCommitte}>
-                                        {item.committee}
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                    {/* {[...Array(n)].map((e, i) =>  */}
+                    <CoordCard data={data.festcoord} title="Fest Coordinators" />
+                    <CoordCard data={data.spons} title="Spons Committee" />
+                    <CoordCard data={data.mpr} title="MPR Committee" />
+                    <CoordCard data={data.webnapp} title="Web & App" />
+                    <CoordCard data={data.events} title="Events Committee" />
+                    <CoordCard data={data.rsp} title="RSP  Committee" />
+                    <CoordCard data={data.hospi} title="Hospitality  Committee" />
+                    <CoordCard data={data.TV} title="Anwesha TV" />
                 </div>
             </div>
         </div>
     )
 }
-
-export default ourteam
