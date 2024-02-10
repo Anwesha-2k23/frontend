dadc603c68dbeec4fbcfc7ec17fc53c307095586
Aritra Bhaduri <92646038+aritroCoder@users.noreply.github.com>
2024-02-10 11:58:37 +0530

    Changed year to 2024

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ const index = () => {
     return (
         <>
             <Head>
-                <title>Anwesha 2023</title>
-                <meta name="description" content="Anwesha 2023" />
+                <title>Anwesha 2024</title>
+                <meta name="description" content="Anwesha 2024" />
                 <link rel="icon" href="./logo_no_bg.svg" />
             </Head>
             <div className={styles.bg}>
